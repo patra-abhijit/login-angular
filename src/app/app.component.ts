a9bd7b6acb80ca1c3c79c0806a0fda779646c409
patra-abhijit <patraabhijit480@gmail.com>
2023-04-27 15:11:44 +0530

    my login page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LoginPage';
+  
+  email:any;
+  password:any;
+  logInUser(): void{
+    if(this.email=="abhi@gmail.com" && this.password=="1234"){
+      console.log("welcome user");
+    }
+    else{
+      alert("user not authorised");
+    }
+  }
 }
